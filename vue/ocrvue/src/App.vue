fc28c60af3af16c6033f68f2ae3c08e882e45fd5
xun-girl <xungirl1024@gmail.com>
2024-05-04 22:28:01 +0800

    z

--- vue/ocrvue/src/App.vue
+++ vue/ocrvue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Hello everybody#2c3e50"/>
 </template>
 
 <script>
@@ -20,7 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2C83DB;
   margin-top: 60px;
 }
 </style>
